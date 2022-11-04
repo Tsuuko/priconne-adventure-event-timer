f6b6d1f99d90cd62df7fe8c85462ed518a29a71f
Tsuuko <23350171+Tsuuko@users.noreply.github.com>
2022-11-04 16:03:33 +0900

    chakra ui追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppComponent } from 'next/dist/shared/lib/router/router';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: AppComponent = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default App;
