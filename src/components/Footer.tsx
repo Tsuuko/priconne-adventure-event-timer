2af06d0c88e6574017470b154e75cdeb284ed750
Tsuuko <23350171+Tsuuko@users.noreply.github.com>
2022-11-05 00:29:44 +0900

    iosのバーとfooterが重ならないようにする

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,23 +6,19 @@ import { GITHUB_REPO_URL, TWITTER_URL } from '../constant';
 import { LinkButton } from './LinkButton';
 export const Footer: React.FC<BoxProps> = (props) => {
   return (
-    <Center {...props} py={2}>
-      <HStack>
-        <LinkButton
-          href={GITHUB_REPO_URL}
-          image={
-            <Image src="/github.svg" alt="github" width="20" height="20" />
-          }
-          text="GitHub"
-        />
-        <LinkButton
-          href={TWITTER_URL}
-          image={
-            <Image src="/twitter.svg" alt="twitter" width="20" height="20" />
-          }
-          text="Twitter"
-        />
-      </HStack>
-    </Center>
+    <HStack justify="center" pt={3} pb={6} {...props}>
+      <LinkButton
+        href={GITHUB_REPO_URL}
+        image={<Image src="/github.svg" alt="github" width="20" height="20" />}
+        text="GitHub"
+      />
+      <LinkButton
+        href={TWITTER_URL}
+        image={
+          <Image src="/twitter.svg" alt="twitter" width="20" height="20" />
+        }
+        text="Twitter"
+      />
+    </HStack>
   );
 };
